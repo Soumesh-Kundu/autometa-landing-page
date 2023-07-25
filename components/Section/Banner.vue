55b9599b23b66d8ce827c661d381afa1f2ec9a93
Soumesh Kundu <iamsoumo26@gmail.com>
2023-07-25 11:09:21 +0530

    different banner for different devices created

--- components/Section/Banner.vue
+++ components/Section/Banner.vue
@@ -2,13 +2,15 @@
     <section class="my-20">
         <div class="flex flex-col items-center max-w-3xl mx-auto">
             <h2 class="px-4 text-3xl font-semibold text-center sm:text-4xl">Comprehensive Expertise</h2>
-            <p class="max-w-xl px-4 mt-5 font-medium text-center text-gray-500 ">We possess extensive experience in working with major CRMs, Finance systems, Databases, SaaS applications, UI/UX, Marketing applications, and other modern systems.</p>
+            <p class="max-w-xl px-4 mt-5 font-medium text-center text-gray-500 ">We possess extensive experience in working
+                with major CRMs, Finance systems, Databases, SaaS applications, UI/UX, Marketing applications, and other
+                modern systems.</p>
         </div>
-        <img src="/images/banner.webp" alt="banner-image" class="w-full my-5"/>
-        <!--<button type="button"
-            class="flex items-center gap-2 px-5 py-3 mx-auto mt-10 text-sm font-medium text-white rounded-md focus:outline-none bg-primary-500">See more templates
-            <Cog6ToothIcon class="w-5 h-5" />
-        </button>-->
+        <picture>
+            <source media="(max-width:648px)" srcset="/images/banner-sm.webp"/>
+            <source media="(max-width: 1024px)" srcset="/images/banner-lg.webp"/>
+            <img src="/images/banner.webp" alt="banner-image" class="w-full my-5" />
+        </picture>
     </section>
 </template>
 
